918cdeb11f6487f64c874934e3497174bef6c5b2
yihuaijiereal <yihuaijiereal@gmail.com>
2016-03-26 19:21:24 +0800

    Kth permutation

--- list/nextPermutation.cc
+++ list/nextPermutation.cc
@@ -26,6 +26,41 @@ public:
 
 		return true;
 	}
+
+	string getPermutation(int n,int k){
+		string s(n,'0');
+		string result;
+		for(int i=0;i<n;i++){
+			s[i]+=i+1;
+		}
+		return kth_permutation(s,k);
+	}
+
+private:
+	int factorial(int n){
+		if(n==1)
+			return 1;
+		else
+			return n*factorial(n-1);
+	}
+
+	template<typename Sequence>
+	Sequence kth_permutation(const Sequence &seq,int k){
+		const int n = seq.size();
+		Sequence S(seq);
+		Sequence result;
+
+		int base = factorial(n-1);
+		--k;
+
+		for(int i=n-1;i>0;k%=base,base/=i,--i){
+			auto a = std::next(S.begin(),k/base);
+			result.push_back(*a);
+			S.erase(a);
+		}
+		result.push_back(S[0]);
+		return result;
+	}
 };
 
 int main(){
@@ -41,4 +76,6 @@ int main(){
 		cout<<vi<<" ";
 	}
 	cout<<endl;
+	string s = solution.getPermutation(8,410);
+	cout<<s<<endl;
 }
