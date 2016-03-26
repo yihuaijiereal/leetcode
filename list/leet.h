41a0d10325878912f0152b4ea9244dd30f9871e7
yihuaijiereal <yihuaijiereal@gmail.com>
2016-03-26 12:53:10 +0800

    head file

---
+++ list/leet.h
@@ -0,0 +1,19 @@
+#ifndef _LEE_H
+#define _LEE_H
+
+#include <iostream>
+using std::cin;using std::cout;using std::endl;
+
+#include <vector>
+using std::vector;
+
+#include <algorithm>
+using std::min;using std::max;
+
+#include <unordered_map>
+using std::unordered_map;
+using std::sort;
+#include <limits>
+
+
+#endif
