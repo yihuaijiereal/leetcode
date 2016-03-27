43a30e3ed1cc22c53ab00cee156d5b6c524a26df
yihj <yihj@yihuaijiereal.local>
2016-03-27 22:50:51 +0800

    trapping rain water

---
+++ list/rain_water.cc
@@ -0,0 +1,63 @@
+#include "leet.h"
+
+class Solution
+{
+public:
+    int trap(const std::vector<int> &A){
+        const int n = A.size();
+        int *max_left = new int[n]();
+        int *max_right = new int[n]();
+
+        for(int i=1;i<n;i++){
+            max_left[i] = max(max_left[i-1],A[i-1]);
+            max_right[n-1-i] = max(max_right[n-i],A[n-i]);
+        }
+
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            int height = min(max_right[i],max_left[i]);
+            if(height>A[i]){
+                sum += height - A[i];
+            }
+        }
+
+        delete[] max_right;
+        delete[] max_left;
+        return sum;
+    }
+
+    int trap2(const std::vector<int> &A){
+        const int n = A.size();
+
+        int max = 0;
+        for(int i=0;i<n;++i){
+            if(A[i]>A[max])
+                max = i;
+        }
+
+        int water = 0;
+        for(int i=0,peak = 0;i<max;++i){
+            if(A[i]>peak)
+                peak = A[i];
+            else
+                water += peak - A[i];
+        }
+        for(int i=n-1,top = 0;i>max;--i){
+            if(A[i]>top)
+                top = A[i];
+            else
+                water += top - A[i];
+        }
+
+        return water;
+    }
+};
+
+int main(){
+    int a[] = {0,1,0,2,1,0,1,3,2,1,2,1};
+    std::vector<int> A(a,a+12);
+    Solution solution;
+    int result = solution.trap(A);
+    int result2 = solution.trap2(A);
+    cout<<result<<" "<<result2<<endl;
+}
